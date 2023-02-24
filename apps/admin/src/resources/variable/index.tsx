c6f2d65d5bc90cd992703dfd4012488edb00de7b
Alexandre Dos Reis <alexandre@finishers.com>
2023-02-24 11:40:10 +0100

    Admin: test mui grid

--- apps/admin/src/resources/variable/index.tsx
+++ apps/admin/src/resources/variable/index.tsx
@@ -8,6 +8,8 @@ import {
   TextInput,
   useRecordContext,
 } from "react-admin";
+import { Grid } from "@mui/material";
+
 import { resources as r } from "../resources.map";
 import { RichTextInput } from "ra-input-rich-text";
 import { VAR_KEY_TYPE } from "@aaw/prisma/react";
@@ -36,9 +38,20 @@ const Form = () => {
   const record = useRecordContext<{ type: string }>();
   return (
     <SimpleForm>
-      <TextInput source="id" label="Clé unique de la varible" disabled />
-      <TextInput source="type" label="Type" disabled />
-      <TextInput source="title" label="Titre" />
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <TextInput
+            source="id"
+            label="Clé unique de la varible"
+            disabled
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <TextInput source="type" label="Type" disabled fullWidth />
+        </Grid>
+      </Grid>
+      <TextInput source="title" label="Titre" fullWidth />
       {record.type === VAR_KEY_TYPE.HTML ? (
         <RichTextInput source="value" label="contenu" />
       ) : null}
