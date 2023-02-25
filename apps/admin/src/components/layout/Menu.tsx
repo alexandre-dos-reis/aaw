3a2c057ce2d9454f51d48fa7088980c38e34fdcf
Alexandre Dos Reis <alexandre@finishers.com>
2023-02-25 19:35:58 +0100

    Admin: Added section in menu

--- apps/admin/src/components/layout/Menu.tsx
+++ apps/admin/src/components/layout/Menu.tsx
@@ -5,13 +5,16 @@ import { Divider } from "@mui/material";
 export const Menu = () => (
   <RaMenu>
     {/* <RaMenu.DashboardItem /> */}
+    <div>Galerie</div>
     <RaMenu.ResourceItem name={r.Artwork.name} />
     <RaMenu.ResourceItem name={r.Category.name} />
     <Divider />
+    <div>Boutique</div>
     <RaMenu.ResourceItem name={r.Product.name} />
     <RaMenu.ResourceItem name={r.ShopCategory.name} />
     <RaMenu.ResourceItem name={r.Purchase.name} />
     <Divider />
+    <div>Réglages</div>
     <RaMenu.ResourceItem name={r.ShippingCost.name} />
     <RaMenu.ResourceItem name={r.AdminVariable.name} />
     {/* <RaMenu.Item to="/custom-route" primaryText="Miscellaneous" /> */}
