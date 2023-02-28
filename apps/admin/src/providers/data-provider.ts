c13e2e425ccc07285421078923c745455c2aa466
Alexandre Dos Reis <alexandre@finishers.com>
2023-02-28 09:01:59 +0100

    Added minio and storage

--- apps/admin/src/providers/data-provider.ts
+++ apps/admin/src/providers/data-provider.ts
@@ -2,9 +2,10 @@ import { Artwork_Category } from "@aaw/prisma";
 import { dataProvider as prismaDataProvider } from "ra-data-simple-prisma";
 import { Identifier, withLifecycleCallbacks } from "react-admin";
 import { resources } from "../resources/resources.map";
+import { env } from "../utils/env";
 
 export const dataProvider = withLifecycleCallbacks(
-  prismaDataProvider("http://localhost:3002/ra"),
+  prismaDataProvider(env.VITE_API_URL),
   [
     {
       // Because Artwork_Category doesn't have an Id
---
+++ apps/admin/src/utils/env.ts
@@ -0,0 +1,4 @@
+export const env = {
+  VITE_API_URL: import.meta.env.VITE_API_URL as string,
+  VITE_IMAGE_URL: import.meta.env.VITE_IMAGE_URL as string,
+};
